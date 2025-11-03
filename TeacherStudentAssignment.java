6e72a77f3bd2564adcff9029e2578b3034e54c42
hcxerry <montaboncherry238@gmail.com>
2025-11-03 18:31:41 +0800

    Update TeacherStudentAssignment.java

--- TeacherStudentAssignment.java
+++ TeacherStudentAssignment.java
@@ -1,57 +1,65 @@
-class Person {
+class Person { //PARENT CLASS
     protected String name;
+    
     protected int age;
+    
     protected String gender;
-
-    public Person(String name, int age, String gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+    
+    
+public Person(String name, int age, String gender) {
+    this.name = name;
+        
+    this.age = age;
+    
+    this.gender = gender;
     }
 
     public void displayInfo() {
         System.out.println("Name: " + name);
+       
         System.out.println("Age: " + age);
+        
         System.out.println("Gender: " + gender);
     }
 }
-
-class Student extends Person {
+class Student extends Person { //STUDENT INHERITS FROM PERSON
     private String studentId;
-
-    public Student(String name, int age, String gender, String studentId) {
-        super(name, age, gender);
-        this.studentId = studentId;
+          public Student(String name, int age, String gender, String studentId) {
+       
+    super(name, age, gender);
+        
+    this.studentId = studentId;
     }
 
     public void displayStudent() {
         System.out.println("--- Student Information ---");
         displayInfo();
+        
         System.out.println("Student ID: " + studentId);
     }
 }
-
 class Course {
     private String courseCode;
+    
     private String courseName;
 
     public Course(String courseCode, String courseName) {
         this.courseCode = courseCode;
         this.courseName = courseName;
     }
-
     public void displayCourse() {
         System.out.println(courseCode + " - " + courseName);
     }
-
     @Override
     public String toString() {
         return courseCode + " - " + courseName;
     }
 }
 
-class Teacher extends Person {
+class Teacher extends Person { //TEACHER INHERITS FROM PERSON
+    
     private String department;
+    
     private Course[] courses; 
 
     public Teacher(String name, int age, String gender, String department, Course[] courses) {
@@ -63,7 +71,9 @@ class Teacher extends Person {
     public void displayTeacher() {
         System.out.println("--- Teacher Information ---");
         displayInfo();
+        
         System.out.println("Department: " + department);
+        
         System.out.println("Courses Handled:");
         for (Course c : courses) {
             System.out.println("    " + c.toString());
@@ -73,23 +83,32 @@ class Teacher extends Person {
 
 public class TeacherStudentAssignment {
     public static void main(String[] args) {
-        // 1. Student example
-        System.out.println("1. Displaying Student Information");
+
+        //OUTPUT
+        System.out.println("1. Example of Student Information");
+        
         Student s = new Student("Ana Santos", 19, "Female", "S1023");
+        
         s.displayStudent();
+        
         System.out.println();
 
-        // 2. Teacher example
-        System.out.println("2. Displaying Teacher Information");
+        System.out.println("2. Example of Teacher Information");
+        
         Course c1 = new Course("CS101", "Programming Fundamentals");
+        
         Course c2 = new Course("CS102", "Object-Oriented Programming");
+        
         Course[] teacherCourses = { c1, c2 };
+        
         Teacher t = new Teacher("Mr. Dela Cruz", 45, "Male", "Computer Studies", teacherCourses);
+        
         t.displayTeacher();
+        
         System.out.println();
 
-        // 3. Displaying a course
-        System.out.println("3. Displaying the course");
+        System.out.println("3. Course");
+        
         c1.displayCourse();
     }
 }
@@ -98,4 +117,5 @@ public class TeacherStudentAssignment {
 // AI THAT USE ChatGPT-5 
 // Conversation of AI 
 // https://chatgpt.com/share/68ff6a33-60e8-8005-9050-4eea8050df5c
+
 
